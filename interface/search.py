0feb01386f4c9dc6d2e7d49d1d68a911b90b1fb7
Ondrej Majerech <oxyd.oxyd@gmail.com>
2012-03-18 23:07:44 +0100

    Finish the refactoring of GUI.

--- interface/search.py
+++
@@ -1,6 +0,0 @@
-from apnsmod import ProofNumberSearch, Game
-
-def makeSearch(board, attacker):
-  '''Make an instance of a ProofNumberSearch using the specified board and attacker.'''
-
-  return ProofNumberSearch(Game(board, attacker))
