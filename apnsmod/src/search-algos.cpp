8b450c9c8a0421ed57429c1cf12eba29478c8c80
Ondrej Majerech <oxyd.oxyd@gmail.com>
2012-07-18 00:02:25 +0200

    Changed epsilon to 0.1.

--- apnsmod/src/search-algos.cpp
+++ apnsmod/src/search-algos.cpp
@@ -1018,7 +1018,7 @@ depth_first_pns::make_limits(
   boost::optional<vertex const&> second_best,
   limits_t parent_limits
 ) {
-  double const EPSILON = 0.5;
+  double const EPSILON = 0.1;
 
   vertex::number_t vertex::* min_num = 
     parent.type == vertex::type_or ? &vertex::proof_number
